65936c0d15722504f03781060a108198207799fb
theKwabena <abbeyemmanueltetteh@gmail.com>
2023-02-16 12:32:04 -0800

    Code for C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/** main is the main function to handle your code
+ * question says return 0
+*/
 
-int main(){
+int main()
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
